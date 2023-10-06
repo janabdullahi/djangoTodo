4ec86c14122385af5583ec16f6537027f3b70c71
abdullahi <jan.ab1517@gmail.com>
2023-10-06 08:25:02 +0100

    add crud model

--- todo/crud/models.py
+++ todo/crud/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class crud(models.Model):
+    title = models.CharField(max_length=150)
+    complete = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
