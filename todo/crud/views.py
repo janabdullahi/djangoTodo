824e092a0246bec4bc0bec61c16aa969b5694609
abdullahi <jan.ab1517@gmail.com>
2023-10-06 08:59:25 +0100

    add forms and use it in template

--- todo/crud/views.py
+++ todo/crud/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from .models import crud
+from .forms import crudForm
+
 # Create your views here.
 
 def index(request):
     cruds = crud.objects.all()
-    context = {'cruds':cruds}
+    form = crudForm()
+
+    context = {'cruds':cruds, 'form':form}
 
     return render(request, 'crud/list.html', context)
